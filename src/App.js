ee09203e6051503b7509930c87c633e01907113b
Slawek <61154596+Ultran@users.noreply.github.com>
2021-08-13 22:56:44 +0200

    Change: add some definition to readme

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Made witch function component</p>
+      <p>Made with function component</p>
       <div className="formFunctionContainer">
         <FunctionComponent />
       </div>
-      <p className="title">Made witch class component</p>
+      <p className="title">Made with class component</p>
       <div className="formClassContainer">
         <ClassComponent />
       </div>
